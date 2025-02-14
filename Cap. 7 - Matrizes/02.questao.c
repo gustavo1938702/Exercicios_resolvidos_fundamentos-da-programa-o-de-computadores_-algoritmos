b5b99309dbb36cf66897f3a9012cb64eaa30285f
Gustavo <gustavo2020ek@gmail.com>
2025-02-14 15:26:15 -0300

    Update and rename 2.questao.c to 02.questao.c

--- Cap. 7 - Matrizes/02.questao.c
+++ Cap. 7 - Matrizes/02.questao.c
@@ -1,6 +1,6 @@
-/*Crie um programa que preencha uma matriz 2 ? 4 com números inteiros, calcule e mostre:
+/*Crie um programa que preencha uma matriz 2 ? 4 com nÃºmeros inteiros, calcule e mostre:
 a quantidade de elementos entre 12 e 20 em cada linha; 
-a média dos elementos pares da matriz.*/
+a mÃ©dia dos elementos pares da matriz.*/
 #include <stdio.h>
 
 int main()
