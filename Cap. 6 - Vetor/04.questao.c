9edfa3061f8fd99b5b9883e6b69e507765a7a2ce
Gustavo <gustavo2020ek@gmail.com>
2025-02-14 15:21:41 -0300

    Update and rename 4.questao.c to 04.questao.c

--- Cap. 6 - Vetor/04.questao.c
+++ Cap. 6 - Vetor/04.questao.c
@@ -1,5 +1,5 @@
-/*Faça um programa que preencha um vetor com quinze elementos inteiros e verifique a existência de elementos 
-iguais a 30, mostrando as posições em que apareceram.*/
+/*FaÃ§a um programa que preencha um vetor com quinze elementos inteiros e verifique a existÃªncia de elementos 
+iguais a 30, mostrando as posiÃ§Ãµes em que apareceram.*/
 #include <stdio.h>
 
 int main()
