16fc9e62f9a1a16cd5454e1f94dd60f9a14de781
Gustavo <gustavo2020ek@gmail.com>
2025-02-14 15:27:25 -0300

    Update and rename 2.questao.c to 02.questao.c

--- Cap. 8 - Sub-rotinas/02.questao.c
+++ Cap. 8 - Sub-rotinas/02.questao.c
@@ -1,4 +1,4 @@
-/*Crie uma sub-rotina que receba três números inteiros como parâmetros, representando horas, minutos e segundos,
+/*Crie uma sub-rotina que receba trÃªs nÃºmeros inteiros como parÃ¢metros, representando horas, minutos e segundos,
 e os converta em segundos. Exemplo: 2h, 40min e 10s correspondem a 9.610 segundos.*/
 
 #include <stdio.h>
