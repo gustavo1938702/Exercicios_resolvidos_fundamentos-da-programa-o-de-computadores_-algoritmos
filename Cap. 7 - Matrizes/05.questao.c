d8dd8bb93f9332975811b4f0be0273ddf32d4539
Gustavo <gustavo2020ek@gmail.com>
2025-02-14 15:24:50 -0300

    Update and rename 5.questao.c to 05.questao.c

--- Cap. 7 - Matrizes/05.questao.c
+++ Cap. 7 - Matrizes/05.questao.c
@@ -1,7 +1,7 @@
 /*Elabore um programa que preencha uma matriz 12 ? 4 com os valores das vendas de uma loja, em que cada
-linha representa um mês do ano e cada coluna representa uma semana do mês. O programa deverá calcular 
+linha representa um mÃªs do ano e cada coluna representa uma semana do mÃªs. O programa deverÃ¡ calcular 
 e mostrar: 
-?? o total vendido em cada mês do ano, mostrando o nome do mês por extenso; 
+?? o total vendido em cada mÃªs do ano, mostrando o nome do mÃªs por extenso; 
 ?? o total vendido em cada semana durante todo o ano; 
 ?? o total vendido pela loja no ano.*/
 #include <stdio.h>
