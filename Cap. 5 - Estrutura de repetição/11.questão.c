845c38ec73a0c2287f62aaa37ccec59d99df0d7a
Gustavo <gustavo2020ek@gmail.com>
2025-02-14 22:34:14 -0300

    Update 11.questão.c

--- Cap. 5 - Estrutura de repetição/11.questão.c
+++ Cap. 5 - Estrutura de repetição/11.questão.c
@@ -2,18 +2,54 @@
 
 int main()
 {
-    float valor_carro;
+    float valor_carro, acrescimo, parcelas, preco_final;
     int i;
 
     printf("Digite o valor do carro: ");
     scanf("%f",&valor_carro);
 
-    printf("\n----------TABELA----------\n\n");
-    printf("Preco final para compra a vista com desconto de 20%%: %.2f",valor_carro * 0.80);
-    printf("\n\nPreco do carro por parcelas:\n\n");
-    for(i = 6; i <= 60; i = i + 6) {
-        printf("%d Parcelas ---- R$%.2f\n\tAcrescimo dos juros: R$%.2f\n\tTotal: R$%.2f\n\n",i ,valor_carro / (float) i, valor_carro * (i / 2.0) / 100.0, valor_carro + valor_carro * (i / 2.0) / 100.0);
-    }
+    printf("\n----------TABELA DE PRECOS----------\n\n");
+    printf("Preco do carro a vista, com desconto de 20%%: %.2f\n\n",valor_carro * 0.80);
 
+    printf("Parcelas | Valor da parcela | Acrescimo | Valor final\n------------------------------------------------------\n");
+    for(i = 6; i <= 60; i = i + 6) {
+        switch(i) {
+            case 6:
+                acrescimo = 0.03;
+            break;
+            case 12:
+                acrescimo = 0.06;
+            break;
+            case 18:
+                acrescimo = 0.09;
+            break;
+            case 24:
+                acrescimo = 0.12;
+            break;
+            case 30:
+                acrescimo = 0.15;
+            break;
+            case 36:
+                acrescimo = 0.18;
+            break;
+            case 42:
+                acrescimo = 0.21;
+            break;
+            case 48:
+                acrescimo = 0.24;
+            break;
+            case 54:
+                acrescimo = 0.27;
+            break;
+            case 60:
+                acrescimo = 0.30;
+            break;
+        }
+
+        preco_final = valor_carro * (1 + acrescimo);
+        parcelas = acrescimo * valor_carro;
+
+        printf("  %d\t |     %.2f\t    | \t  %.0f\t|  %.2f\n",i, parcelas, acrescimo * 100, preco_final);
+    }
     return 0;
 }
